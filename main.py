575c82b2fb3f9a7de10e6b24a7f28ca45be2a00c
rizqita <rizqitaamalia5@gmail.com>
2024-09-21 15:30:38 +0700

    pelcod 21 sep 2024

---
+++ main.py
@@ -0,0 +1,15 @@
+# nilai = int(input("Masukkan nilai: ")) 
+
+# if (nilai > 80):
+#     print("selamat anda lulus")
+# elif(nilai > 90 and nilai < 100):
+#     print("selamat anda lulus, dengan nilai yang memuaskan")
+# else:
+#     print("anda masih belum lulus tes, semangat dan coba lagi")
+
+# [FUNCTION]
+#def
+def sayHello():
+    print("Hello martha")
+
+sayHello()
